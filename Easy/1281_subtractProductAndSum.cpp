e2af70ad633921181e90aa4a298333291026f663
Kavya Gupta <kavygupta0605@gmail.com>
2026-04-02 00:18:36 +0530

    Add files via upload

---
+++ Easy/1281_subtractProductAndSum.cpp
@@ -0,0 +1,22 @@
+// LeetCode 1281 - Subtract the Product and Sum of Digits of an Integer
+// Difficulty: Easy
+
+#include <iostream>
+using namespace std;
+
+class Solution {
+public:
+    int subtractProductAndSum(int n) {
+        int prod = 1;
+        int sum = 0;
+
+        while (n != 0) {
+            int digit = n % 10;
+            prod = prod * digit;
+            sum = sum + digit;
+            n = n / 10;
+        }
+        int ans = prod - sum;
+        return ans;
+    }
+};
