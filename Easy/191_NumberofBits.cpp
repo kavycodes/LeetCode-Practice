7e037a8e1bd3bcfe65ec649543a6b811f9aad7b9
Kavya Gupta <kavygupta0605@gmail.com>
2026-04-03 21:36:51 +0530

    Add files via upload

---
+++ Easy/191_NumberofBits.cpp
@@ -0,0 +1,16 @@
+// LeetCode 191 - Number of Bits
+// Difficulty: Easy
+
+class Solution {
+public:
+    int hammingWeight(int n) {
+        int count=0;
+        while(n!=0){
+            if(n&1){
+                count++;
+            }
+            n= n>>1;
+        }
+        return count;
+    }
+};
